2f409cad6a687905f8138bad40688f89ba299f45
Matheus <matheus.mello@vetorzero.com.br>
2018-10-01 16:39:10 -0300

    colocando as coisas do yan

---
+++ inter/Assets/Scripts/MaterialControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Substance.Game;
+
+
+public class MaterialControler : MonoBehaviour
+{
+	
+	public SubstanceGraph WaterSubstance;
+	private float _value=0;
+	private bool _right;
+	public float Velocity;
+
+	
+	private void Update()
+	{
+		if (!_right)
+		{
+			_value += Time.deltaTime/Velocity;
+			if (_value>1)
+			{
+				_right = true;
+			}
+		}
+		else
+		{
+			_value -= Time.deltaTime/Velocity;
+			if (_value<0)
+			{
+				_right = false;
+			}
+		}
+		WaterSubstance.SetInputFloat("disorder", _value);
+		WaterSubstance.QueueForRender();
+	}
+
+}
