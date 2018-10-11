ca86080a2929c21e1aa9293597b553f720d57120
Unknown <yan_sao.paulo@hotmail.com>
2018-10-11 14:54:19 -0300

    SimplesPause e GameFlow

---
+++ inter/Assets/Yan/Scripts/GameMenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuManager : MonoBehaviour {
+
+    public GameObject PausePanel;
+	
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!PausePanel.activeSelf)
+            {
+                OpenPause();
+            }else
+            {
+                ClosePause();
+            }           
+        }
+	}
+
+    public void OpenPause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+    }
+
+    public void ClosePause()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+    }
+}
