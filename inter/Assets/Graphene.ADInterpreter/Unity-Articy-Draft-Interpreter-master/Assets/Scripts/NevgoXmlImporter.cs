215bc3725cb7cb9cc13417fc81f9e5e41df23276
Unknown <junqueirademello@gmail.com>
2018-10-13 18:57:23 -0300

    foiiiii porrrrraaaaaaa

--- inter/Assets/Graphene.ADInterpreter/Unity-Articy-Draft-Interpreter-master/Assets/Scripts/NevgoXmlImporter.cs
+++ inter/Assets/Graphene.ADInterpreter/Unity-Articy-Draft-Interpreter-master/Assets/Scripts/NevgoXmlImporter.cs
@@ -1,6 +1,5 @@
-﻿using System.IO;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
 using Boo.Lang;
@@ -40,11 +39,57 @@ namespace Graphene.ADInterpreter.Assets.Scripts
                 var str = text.Text.LocalizedString.Text.Replace("<html><head><style>#s0 {text-align:left;} #s1 {font-size:11pt;} </style></head><body><p id=\"s0\"><span id=\"s1\">", "");
                 str = str.Replace("</span></p></body></html>", "");
                 str = str.Replace("</span><span id=\"s1\">", "");
-                
-                Debug.Log(str);
+                str = DecodeHtmlChars(str);
+              Debug.Log(str);
             }
                 
         }
+        string DecodeHtmlChars(string aText)
+        {
+            string[] parts = aText.Split(new string[]{"&#"}, StringSplitOptions.None);
+            for (int i = 1; i < parts.Length; i++)
+            {
+                int n = parts[i].IndexOf(';');
+                string number = parts[i].Substring(0,n);
+                
+                switch (number)
+                {
+                     case "225" :
+                         number = "á";
+                         break;
+                     case "243" : 
+                         number = "ó";
+                         break;
+                     case "227" : 
+                         number = "ã";
+                         break;
+                     case "237" : 
+                         number = "í";
+                         break;
+                     case "233" :
+                         number = "é";
+                         break;
+                     case "234" : 
+                         number = "ê";
+                         break;
+                     case "231" : 
+                         number = "ç";
+                         break;
+                     case "245" : 
+                         number = "õ";
+                         
+                         break;
+                }
+                try
+                {
+                    
+                    parts[i] = (number) + parts[i].Substring(n+1);
+               } catch {}
+            }
+            return String.Join("",parts);
+        }
+        
+       
         
         
     }
