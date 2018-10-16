82b87d3d8076593d321632afa92a78a9dd78cf05
Unknown <fgalinskas@gmail.com>
2018-10-16 17:13:00 -0300

    Update de meshs

--- inter/Assets/Yan/Scripts/GameMenuManager.cs
+++ inter/Assets/Yan/Scripts/GameMenuManager.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class GameMenuManager : MonoBehaviour {
 
     public GameObject PausePanel;
-	
+    public PostProcessingProfile efeito;
+    public float focus_distance;
+    public float focal_length;
 
-	void Update () {
+    private void Start()
+    {
+        var focus = efeito.depthOfField.settings;
+        focus.focusDistance = focus_distance = 10f;
+        efeito.depthOfField.settings = focus;
+
+        var focal = efeito.depthOfField.settings;
+        focal.focalLength = focal_length = 5f;
+        efeito.depthOfField.settings = focal;
+    }
+    void Update () {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (!PausePanel.activeSelf)
             {
+                
                 OpenPause();
             }else
             {
+                
+                    //cria uma variavel para armazenzar o valor
+                    var focus = efeito.depthOfField.settings;
+                    focus.focusDistance = focus_distance = 10f;
+                    efeito.depthOfField.settings = focus;
+
+                    var focal = efeito.depthOfField.settings;
+                    focal.focalLength = focal_length = 5f;
+                    efeito.depthOfField.settings = focal;
+                
                 ClosePause();
             }           
         }
@@ -25,6 +49,18 @@ public class GameMenuManager : MonoBehaviour {
         Cursor.lockState = CursorLockMode.None;
         Time.timeScale = 0;
         PausePanel.SetActive(true);
+
+        //ativa o efeito de desfoque
+        
+            //cria uma variavel para armazenzar o valor
+            var focus = efeito.depthOfField.settings;
+            focus.focusDistance = focus_distance = 0.1f;
+            efeito.depthOfField.settings = focus;
+
+            var focal = efeito.depthOfField.settings;
+            focal.focalLength = focal_length = 300f;
+            efeito.depthOfField.settings = focal;
+        
     }
 
     public void ClosePause()
@@ -32,5 +68,14 @@ public class GameMenuManager : MonoBehaviour {
         Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1;
         PausePanel.SetActive(false);
+
+        //cria uma variavel para armazenzar o valor
+        var focus = efeito.depthOfField.settings;
+        focus.focusDistance = focus_distance = 10f;
+        efeito.depthOfField.settings = focus;
+
+        var focal = efeito.depthOfField.settings;
+        focal.focalLength = focal_length = 5f;
+        efeito.depthOfField.settings = focal;
     }
 }
