db690f472568124e1ba3f6262f80b0e1e106d469
Unknown <fgalinskas@gmail.com>
2018-11-02 15:07:34 -0300

    focus ajustado no complexo e teste luz com dummy

---
+++ inter/Assets/Scripts/camera_focus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class camera_focus : MonoBehaviour
+{
+
+    [SerializeField]
+    private LayerMask rayMask;
+
+    [SerializeField]
+    private float maxiDistance;
+
+    private RaycastHit hit;
+
+    [SerializeField]
+    private Transform depthOfFieldObject;
+
+    private void Update()
+    {
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxiDistance, rayMask))
+        {
+            depthOfFieldObject.position = hit.point;
+
+        }
+        else {
+            depthOfFieldObject.position = transform.position + transform.forward * maxiDistance * 0.5f;
+        }
+    }
+
+
+
+}
