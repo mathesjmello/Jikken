021a7737c058ddfc7923336cc395c47e7e6676e2
Unknown <fgalinskas@gmail.com>
2018-11-27 12:14:33 -0200

    UPdate com cheat menu

---
+++ inter/Assets/Scripts/managerCheat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class managerCheat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void cena1(int quest) {
+        //house exterior
+        Persistence.Quest = 0;        
+
+    }
+
+    public void LoadScene(string Scene)
+    {
+        MyLoad.Loading(Scene);
+    }
+}
